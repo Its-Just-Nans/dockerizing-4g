f5edbc930586b7a23a61c2f36202e4cb6cc6071f
n4n5 <its.just.n4n5@gmail.com>
2024-04-06 20:48:11 +0200

    clean

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ What about [open5gs](https://github.com/open5gs/open5gs) and [srsRAN](https://gi
 
 ```sh
 docker compose up --force-recreate --build
-```
 
-```sh
+# rebuild without cache or just docker system prune -a
+docker compose build --no-cache
+
+# see log of service (ue)
 docker compose logs ue -f
 ```
--- core/upf/upf.sh
+++ core/upf/upf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+
 iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
-ufw disable
+ip6tables -t nat -A POSTROUTING -s 2001:db8:cafe::/48 ! -o ogstun -j MASQUERADE
 
 /app/open5gs/install/bin/open5gs-upfd
